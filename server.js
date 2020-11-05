814a52a94fea1048ba9daa05cacf1ac8e6eb15ac
hngdng <65930682+hngdngng@users.noreply.github.com>
2020-11-04 22:25:41 -0500

    inital commit, base of application

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+// const app = express();
+// const routes = require("./routes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+// API routes
+// app.use(routes);
+
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
