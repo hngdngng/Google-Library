6cf7d831dd6db1137d0d19a14e9dc4cfd1314302
hngdng <65930682+hngdngng@users.noreply.github.com>
2020-11-06 23:48:34 -0500

    added routes, controllers

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 const PORT = process.env.PORT || 3001;
-// const app = express();
-// const routes = require("./routes");
+const app = express();
+const routes = require("./routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+// Static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Connect to the Mongo DB
+// Mongo DB connection
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
   {
@@ -22,8 +21,7 @@ mongoose.connect(
 );
 
 // API routes
-// app.use(routes);
-
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
